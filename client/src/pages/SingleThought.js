025b154d188cfd6ca4b73df0eb6fecfa1ab740ac
Chris Porter <cap8095@gmail.com>
2022-07-14 18:11:32 -0400

    all kinds of stuff

---
+++ client/src/pages/SingleThought.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import { useParams } from "react-router-dom";
+import ReactionList from "../components/ReactionList";
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+  console.log(thoughtId);
+  const { data, loading } = useQuery(QUERY_THOUGHTS, {
+    variables: { id: thoughtId },
+  });
+  const thought = data?.thought || {};
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {thought.username}
+          </span>{" "}
+          thought on createdAt
+        </p>
+        <div className="card-body">
+          <p>{thought.thoughtText}</p>
+        </div>
+      </div>
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+    </div>
+  );
+};
+
+export default SingleThought;
